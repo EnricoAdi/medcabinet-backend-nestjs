acc67e6c92f46877968446684a58cce918726d3a
Enrico <enricoadi49@gmail.com>
2024-05-20 22:14:54 +0700

    cors: fix cors

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api");
-  app.enableCors({
-    origin: "https://medcabinet-green.vercel.app/",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true
+  // app.enableCors({
+  //   origin: "https://medcabinet-green.vercel.app/",
+  //   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  //   preflightContinue: false,
+  //   optionsSuccessStatus: 204,
+  //   credentials: true
+  // });
+  app.use(function (request: Request, response: Response, next: NextFunction) {
+    response.setHeader('Access-Control-Allow-Origin', 'https://medcabinet-green.vercel.app');
+    next();
   });
   app.useGlobalPipes(new ValidationPipe({
     stopAtFirstError: true,
