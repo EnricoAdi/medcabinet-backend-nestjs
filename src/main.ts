f0abaff861de714e897a53754f2c6d4df8633a05
Enrico <enricoadi49@gmail.com>
2024-05-20 21:53:04 +0700

    deploy: set cors options

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api");
-  app.enableCors()
+  app.enableCors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true
+  });
   app.useGlobalPipes(new ValidationPipe({
     stopAtFirstError: true,
     transformOptions: {
