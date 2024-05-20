a71628cb053c228b9fb556ae3f0b730ad86d5348
Enrico <enricoadi49@gmail.com>
2024-05-20 21:58:28 +0700

    cors: edit origin

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api");
   app.enableCors({
-    origin: "*",
+    origin: "https://medcabinet-green.vercel.app/",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     optionsSuccessStatus: 204,
