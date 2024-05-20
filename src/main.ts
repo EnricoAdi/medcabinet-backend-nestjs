0aaf9e40d1c99333b7001061d087e30918d31149
Enrico <enricoadi49@gmail.com>
2024-05-20 22:27:42 +0700

    fix: add cors true on nestfactory (?)

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,19 @@ import { ValidationPipe } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix("api");
-  // app.enableCors({
-  //   origin: "https://medcabinet-green.vercel.app/",
-  //   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  //   preflightContinue: false,
-  //   optionsSuccessStatus: 204,
-  //   credentials: true
-  // });
-  app.use(function (request: Request, response: Response, next: NextFunction) {
-    response.setHeader('Access-Control-Allow-Origin', 'https://medcabinet-green.vercel.app');
-    next();
+  app.enableCors({
+    origin: "https://medcabinet-green.vercel.app",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true
   });
+  // app.use(function (request: Request, response: Response, next: NextFunction) {
+  //   response.setHeader('Access-Control-Allow-Origin', 'https://medcabinet-green.vercel.app');
+  //   next();
+  // });
   app.useGlobalPipes(new ValidationPipe({
     stopAtFirstError: true,
     transformOptions: {
