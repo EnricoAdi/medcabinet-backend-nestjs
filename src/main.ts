965a376f26b171e33ea272554a24a4de3dbc600b
Enrico <enricoadi49@gmail.com>
2024-05-21 10:22:03 +0700

    fix: cors edit main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: {
     origin: "*",
     methods: 'GET,OPTIONS,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+    allowedHeaders: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Bearer, Authorization, Access-Control-Allow-Origin, Access-Control-Allow-Methods, Access-Control-Allow-Headers, Access-Control-Allow-Credentials, Access-Control-Expose-Headers, Access-Control-Max-Age, Access-Control-Request-Headers, Access-Control-Request-Method',
     optionsSuccessStatus: 200,
     credentials: true,
 
